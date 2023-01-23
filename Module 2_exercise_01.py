0c8980bdeac0430cc17cc6cf16ee3e022d533ae5
Michaelrt86 <Michaelrt867@gmail.com>
2023-01-23 18:49:26 -0500

    Cleaning up Repository

--- Module 2_exercise_01.py
+++
@@ -1,7 +0,0 @@
-#Module 2 Exercise 1
-#Write a script that takes in a string from the user. Print the string out backwards.
-
-print('Enter a String: ', end= "")
-userInput = input()
-userInput = userInput[::-1]
-print(userInput)
