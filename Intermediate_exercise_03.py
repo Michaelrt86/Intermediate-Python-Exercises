bb500de439481cf2a4828db3c02063a78dd08c4f
Michaelrt86 <Michaelrt867@gmail.com>
2023-01-23 16:17:53 -0500

    Intermediate_exercise_03 finished!

---
+++ Intermediate_exercise_03.py
@@ -0,0 +1,10 @@
+def functionReturn(tempString):
+    userInput = tempString.lower().replace(' ', '')
+    dictList = {}
+    for x in range(0, len(userInput)):
+        dictList[userInput[x]] = userInput.count(userInput[x])
+    return dictList
+
+print('Enter a string: ', end= '')
+userWord = input()
+print(functionReturn(userWord))
