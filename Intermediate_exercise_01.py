26bc2951e9bb83962f1f431c7b8e1a4f9f73cca2
Michaelrt86 <Michaelrt867@gmail.com>
2023-01-22 23:55:22 -0500

    Finished Intermediate Exercise 1!

---
+++ Intermediate_exercise_01.py
@@ -0,0 +1,13 @@
+def get_unique_list(tempList):
+    newList = tempList
+    y = 0
+    while y < len(newList):
+        if newList.count(newList[y]) > 1:
+            newList.remove(newList[y])
+        if y == len(newList) - 1:
+            break
+        y = y + 1
+
+my_list = [1, 2, 3, 2, 1, 4]
+unique_list = get_unique_list(my_list)
+print(str(unique_list))
