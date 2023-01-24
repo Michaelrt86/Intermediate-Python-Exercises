b0318db99950e38930cc73638ca0ad2929cf975b
Michaelrt86 <Michaelrt867@gmail.com>
2023-01-23 21:33:59 -0500

    Intermediate Python exercises 2 exercise1 progress

---
+++ exercise_01/BankAccount.py
@@ -0,0 +1,15 @@
+class BankAccount: 
+    
+
+    def __init__(self, name, balance):
+            self.name = name
+            self.balance = balance
+
+    def deposit(self):
+        account_balance = account_balance + self
+
+    def withdraw(self):
+        account_balance = account_balance - self
+
+    def get_balance(self):
+        return self.name + ' has a balance of ' + self.balance
---
+++ exercise_01/main.py
@@ -0,0 +1,3 @@
+import BankAccount
+
+myAccount = BankAccount()
